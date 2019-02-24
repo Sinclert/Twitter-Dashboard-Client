742baaa7f4ed35d8e19a2a6b61661b924c849041
Sinclert <sinclert@hotmail.com>
2019-02-24 18:00:45 +0100

    Creating Stream page basic structure

--- src/pages/stream/Stream.js
+++ src/pages/stream/Stream.js
@@ -1,1 +1,29 @@
-/* Example */
+/* encoding: utf-8 */
+
+import React, { Component } from "react";
+import { Grid, Segment } from "semantic-ui-react";
+import './Stream.css';
+
+
+export default class Stream extends Component {
+
+    render() {
+        return (
+            <Grid className="stream-layout">
+                <Grid.Row stretched className="stream-header">
+                    <Grid.Column width={16}>
+                        <Segment>Header</Segment>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row stretched className="stream-panel">
+                    <Grid.Column width={3}>
+                        <Segment>Details</Segment>
+                    </Grid.Column>
+                    <Grid.Column width={13}>
+                        <Segment>Panel</Segment>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
+    }
+}
