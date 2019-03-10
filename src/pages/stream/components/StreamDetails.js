b34d0ca14559829e12ac03bd5db147f0faf0409f
Sinclert <sinclert@hotmail.com>
2019-03-10 17:17:51 +0100

    StreamDetails first version

--- src/pages/stream/components/StreamDetails.js
+++ src/pages/stream/components/StreamDetails.js
@@ -1,14 +1,41 @@
 /* encoding: utf-8 */
 
 import React, { Component } from "react";
-import { Segment } from "semantic-ui-react";
+import { Icon, Image, List, Segment } from "semantic-ui-react";
 
 
 export default class StreamDetails extends Component {
 
     render() {
         return (
-            <Segment>Details</Segment>
+            <Segment>
+                <List horizontal className="details-list">
+                    <List.Item className="details-item">
+                        <Image avatar>
+                            <Icon circular inverted color="blue" name="map marker"/>
+                        </Image>
+                        <List.Content>
+                            <List.Header>Location:</List.Header>
+                        </List.Content>
+                    </List.Item>
+                    <List.Item className="details-item">
+                        <Image avatar>
+                            <Icon circular inverted color="blue" name="filter"/>
+                        </Image>
+                        <List.Content>
+                            <List.Header>Filter term:</List.Header>
+                        </List.Content>
+                    </List.Item>
+                    <List.Item className="details-item">
+                        <Image avatar>
+                            <Icon circular inverted color="blue" name="options"/>
+                        </Image>
+                        <List.Content>
+                            <List.Header>Simultaneous results:</List.Header>
+                        </List.Content>
+                    </List.Item>
+                </List>
+            </Segment>
         );
     }
 }
