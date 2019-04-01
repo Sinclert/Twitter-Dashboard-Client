5ce5f0ac36f5a9771200640d5d68a447fb9fdfe5
Sinclert <sinclert@hotmail.com>
2019-04-01 21:41:57 +0200

    StreamGraphs first version

--- src/pages/stream/components/StreamGraphs.js
+++ src/pages/stream/components/StreamGraphs.js
@@ -1,14 +1,46 @@
 /* encoding: utf-8 */
 
-import { Component } from "react";
+import React, { Component } from "react";
+import { Bar, Line } from "react-chartjs-2"
+import { Grid, Segment } from "semantic-ui-react"
+
+
+// Example
+const data= {
+     labels: ["January", "February", "March", "April", "May", "June", "July"],
+     datasets: [{
+         label: "My First dataset",
+         backgroundColor: 'rgb(0, 120, 200)',
+         borderColor: 'rgb(0, 120, 200)',
+         data: [0, 10, 5, 2, 20, 30, 45],
+     }]
+};
 
 
 export default class StreamGraphs extends Component {
 
     render() {
         return (
-            "Test"
+            <Segment>
+                <Grid>
+                    <Grid.Row stretched>
+                        <Grid.Column width={8}>
+                            <Line data={data} width={600} height={250}/>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Bar data={data} width={600} height={250}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row stretched>
+                        <Grid.Column width={8}>
+                            <Line data={data} width={600} height={250}/>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Bar data={data} width={600} height={250}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
         );
     }
 }
-
