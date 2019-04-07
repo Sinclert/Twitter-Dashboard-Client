580e99a3458aa0ae10a7e47ddd94d1d046615bc4
Sinclert <sinclert@hotmail.com>
2019-04-07 14:50:26 +0200

    Creating map layers

--- src/pages/stream/components/StreamMap.js
+++ src/pages/stream/components/StreamMap.js
@@ -1,11 +1,37 @@
 /* encoding: utf-8 */
 
 import React, { Component } from "react";
-import { Map, TileLayer } from "react-leaflet";
+import { CircleMarker, FeatureGroup, LayersControl, Map, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 
 export default class StreamMap extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            center: [37.765248, -122.402676],
+            zoom: 10,
+            tweets: {
+                android: {
+                    color: "green",
+                    data: [{x_coords: 37.802416, y_coords: -122.399547, text: "Example"}],
+                },
+                iphone: {
+                    color: "blue",
+                    data: [],
+                },
+                laptop: {
+                    color: "red",
+                    data: [],
+                },
+                other: {
+                    color: "grey",
+                    data: [],
+                },
+            },
+        };
+    }
 
     componentDidMount () {
         const map = this.refs.map.leafletElement;
@@ -13,13 +39,37 @@ export default class StreamMap extends Component {
     }
 
     render() {
+        const layers = Object.keys(this.state.tweets);
+
         return (
             // The "ref" prop is necessary to obtain the created instance
-            <Map center={[51.505, -0.09]} zoom={10} ref="map">
+            <Map center={this.state.center} zoom={this.state.zoom} ref="map">
+
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="<a href=http://osm.org/copyright>OpenStreetMap</a>"
                 />
+
+                <LayersControl>
+                    {layers.map((layerName, index) =>
+                        <LayersControl.Overlay
+                            key={index}
+                            name={layerName}>
+                            <FeatureGroup color="purple">
+                                {this.state.tweets[layerName].data.map((tweet, index) =>
+                                    <CircleMarker
+                                        key={index}
+                                        center={[tweet.x_coords, tweet.y_coords]}
+                                        radius={2}>
+                                        <Popup>
+                                            <span>{tweet.text}</span>
+                                        </Popup>
+                                    </CircleMarker>
+                                )}
+                            </FeatureGroup>
+                        </LayersControl.Overlay>
+                    )}
+                </LayersControl>
             </Map>
         );
     }
