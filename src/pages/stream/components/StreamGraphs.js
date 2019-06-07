fc2f369392455552ad12275309c9d5ceebfd2183
Sinclert <sinclert@hotmail.com>
2019-06-07 21:51:19 +0200

    Implement 1st version of the Pie and Lines graph

--- src/pages/stream/components/StreamGraphs.js
+++ src/pages/stream/components/StreamGraphs.js
@@ -1,42 +1,95 @@
 /* encoding: utf-8 */
 
 import React, { Component } from "react";
-import { Bar, Line } from "react-chartjs-2"
+import { Doughnut, Line } from "react-chartjs-2"
 import { Grid, Segment } from "semantic-ui-react"
-
-
-// Example
-const data = {
-     labels: ["January", "February", "March", "April", "May", "June", "July"],
-     datasets: [{
-         label: "My First dataset",
-         backgroundColor: 'rgb(0, 120, 200)',
-         borderColor: 'rgb(0, 120, 200)',
-         data: [0, 10, 5, 2, 20, 30, 45],
-     }]
-};
 
 
 export default class StreamGraphs extends Component {
 
+
+    constructor(props) {
+        super(props);
+        this.sourceLabels = ['Android', 'iPhone', 'Web', 'Other'];
+        this.sentimentLabels = ['Positive', 'Negative', 'Neutral'];
+    }
+
+
+    updateSourceCounters(streamData) {
+        let newCounters = [];
+
+        this.sourceLabels.forEach((source) => {
+            let counter = 0;
+            streamData.forEach((tweet) => {
+                if (tweet.source.toLowerCase() === source.toLowerCase()) {
+                    counter++;
+                }
+            });
+            newCounters.push(counter);
+        });
+        return newCounters;
+    }
+
+
+    updateSentimentCounters(streamData) {
+        let newCounters = [];
+
+        this.sentimentLabels.forEach((source) => {
+            let counter = 0;
+            streamData.forEach((tweet) => {
+                if (tweet.label.toLowerCase() === source.toLowerCase()) {
+                    counter++;
+                }
+            });
+            newCounters.push(counter);
+        });
+
+        return newCounters;
+    }
+
+
     render() {
+        const { streamData } = this.props;
+
         return (
-            <Segment>
+            <Segment padded>
                 <Grid>
-                    <Grid.Row stretched>
+                    <Grid.Row>
                         <Grid.Column width={8}>
-                            <Line data={data} width={600} height={250}/>
+                            <Doughnut
+                                width={400}
+                                height={250}
+                                data={{
+                                    datasets: [{
+                                        data: this.updateSourceCounters(streamData),
+                                        backgroundColor: [
+                                            'rgb(000, 150, 000)',
+                                            'rgb(200, 200, 200)',
+                                            'rgb(000, 150, 200)',
+                                            'rgb(100, 100, 100)',
+                                        ]
+                                    }],
+                                    labels: this.sourceLabels,
+                                }}
+                            />
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Bar data={data} width={600} height={250}/>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row stretched>
-                        <Grid.Column width={8}>
-                            <Line data={data} width={600} height={250}/>
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Bar data={data} width={600} height={250}/>
+                            <Line
+                                width={400}
+                                height={250}
+                                data={{
+                                    datasets: [{
+                                        label: 'Sentiment',
+                                        data: this.updateSentimentCounters(streamData),
+                                        backgroundColor: [
+                                            'rgb(000, 150, 000)',
+                                            'rgb(150, 000, 000)',
+                                            'rgb(100, 100, 100)',
+                                        ],
+                                    }],
+                                    labels: this.sentimentLabels,
+                                }}
+                            />
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
